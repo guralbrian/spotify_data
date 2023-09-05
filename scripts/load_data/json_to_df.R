bfa1ff8f25d5a4b6b9f071d1b8e5353380d52d42
Brian Gural <guralbrian@gmail.com>
2023-09-05 17:40:49 -0400

    First pass at setting up directories. Scripts to format away from JSON

---
+++ scripts/load_data/json_to_df.R
@@ -0,0 +1,17 @@
+# This script is meant to be the first step in our analysis pipeline
+# It serves to load the JSON files of listening history and convert them to a format I'm more comfortable with: dataframes
+
+library("rjson")
+library("tidyverse")
+
+# List all files that match the pattern "StreamingHistory*.json" in the folder
+files_to_load <- list.files("data/raw/MyData/", pattern = "StreamingHistory.*\\.json$", full.names = TRUE)
+
+# Load and merge them into a single tibble
+merged_df <- files_to_load %>%
+  lapply(jsonlite::fromJSON) %>%  
+  bind_rows() %>%
+  as_tibble()
+
+# Save the data
+write.csv(merged_df, "data/raw/listen_history_1year.csv")
